d4ec4b32860a1b9625dcb846d8edeff1bca0be19
coencuppen <74605227+coencuppen@users.noreply.github.com>
2022-04-06 14:28:46 +0200

    deleting builds and logs

--- build/receive_node/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/coenc/Documents/GitHub/Infrastructuur/src/receive_node
-# Build directory: C:/Users/coenc/Documents/GitHub/Infrastructuur/build/receive_node
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/send_node/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/coenc/Documents/GitHub/Infrastructuur/src/send_node
-# Build directory: C:/Users/coenc/Documents/GitHub/Infrastructuur/build/send_node
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
