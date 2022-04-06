1c5c82d679cfd583ccf17f36bfd3701112d80666
coencuppen <74605227+coencuppen@users.noreply.github.com>
2022-04-06 14:23:17 +0200

    Documentatie

--- src/send_node/src/main.cpp
+++ src/send_node/src/main.cpp
@@ -14,25 +14,31 @@ using namespace std::chrono_literals;
 class send_nodes : public rclcpp::Node
 {
   public:
-    send_nodes()
-    : Node("minimal_publisher"), count_(0)
+
+    send_nodes() : Node("minimal_publisher")
     {
       publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
       timer_ = this->create_wall_timer(
-      500ms, std::bind(&send_nodes::timer_callback, this));
+      	500ms, std::bind(&send_nodes::timer_callback, this)
+	  );
     }
 
   private:
     void timer_callback()
     {
-      auto message = std_msgs::msg::String();
-      message.data = "Hello, world! " + std::to_string(count_++);
-      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-      publisher_->publish(message);
+		if(count_ < 10){
+			auto message = std_msgs::msg::String();
+			message.data = "test";
+			RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+			publisher_->publish(message);
+			count_ ++;
+		}else{
+			rclcpp::shutdown();
+		}
     }
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-    size_t count_;
+    size_t count_ = 0;
 };
 
 int main(int argc, char * argv[])
